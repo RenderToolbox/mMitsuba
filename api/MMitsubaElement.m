07b809bf0f6618d4d5d6877223520430cd9b3562
Ben Heasly <benjamin.heasly@gmail.com>
2016-04-13 16:57:14 -0400

    hack: call property filename when non-string and doesn't start with a number

--- api/MMitsubaElement.m
+++ api/MMitsubaElement.m
@@ -39,7 +39,59 @@ classdef MMitsubaElement < MMitsubaNode
             
             % optional nested elements
             s = self.appendEach(s, self.nested);
-        end        
+        end
+        
+        function property = setProperty(self, name, type, value)
+            % Add or update a nested property with the given name.
+            %   If this element already contains a nested property with the
+            %   given name, it will be updated with the given type and
+            %   value. Otherwise, a new nested property will be added.
+            %
+            %   By default the given value will have the attribute name
+            %   "value".  If the given type is not string nor boolean, and
+            %   the given value doesn't start with a number, then the
+            %   attribute name "filename" will be used instead.  This is a
+            %   hack so that callers can treat spectrum strings ('400:1
+            %   410:1 420:2 ...') and spectrum file names
+            %   ('my-spectrum.spd') interchangeably.
+            property = self.find(name);
+            
+            if ischar(value) ...
+                    && ~strcmp('string', type) ...
+                    && ~strcmp('boolean', type) ...
+                    && 0 == numel(sscanf(value, '%f'))
+                attributeName = 'filename';
+            else
+                attributeName = 'value';
+            end
+            
+            if isempty(property)
+                property = MMitsubaProperty.withData(name, type, ...
+                    attributeName, value);
+                self.append(property);
+                return;
+            end
+            
+            property.setData(attributeName, value);
+            property.type = type;
+        end
+        
+        function [value, type] = getProperty(self, name)
+            % Locate a nested property with the given name.
+            %   If a nested property with the given name exists, finds and
+            %   returns its "value" data and type.  Otherwise returns [].
+            
+            property = self.find(name);
+            
+            if isempty(property)
+                value = [];
+                type = [];
+                return;
+            end
+            
+            value = property.getData('value');
+            type = property.type;
+        end
     end
     
     methods (Static)
